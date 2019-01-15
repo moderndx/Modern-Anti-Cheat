12d55bafcbbfcd40912380d71e606abeab6906aa
DEADMONSTOR <DEADMONSTOR69@gmail.com>
2019-01-15 20:48:57 +0000

    Initial fix

---
+++ lua/autorun/client/cl_mac.lua
@@ -0,0 +1,387 @@
+// == LOCALIZING
+local m_saved_os = jit.os
+local convar_meta = FindMetaTable( "ConVar" )
+local cusercmd_meta = FindMetaTable( "CUserCmd" )
+local convar_get_string = convar_meta.GetString
+local cusercmd_set_view_angles = cusercmd_meta.SetViewAngles
+local net_recieve = net.Receive
+local net_start = net.Start
+local net_writebool = net.WriteBool
+local net_writetable = net.WriteTable
+local net_writestring = net.WriteString
+local net_sendtoserver = net.SendToServer
+local net_readtable = net.ReadTable
+local net_readbool = net.ReadBool
+local net_readstring = net.ReadString
+local create_client_convar = CreateClientConVar
+local concommand_gettable = concommand.GetTable
+local concommand_add = concommand.Add
+local get_convar = GetConVar
+local debug_getinfo = debug.getinfo
+local debug_getupvalue = debug.getupvalue
+local debug_traceback = debug.traceback
+local vgui_create = vgui.Create
+local hook_add = hook.Add
+local module_require = require
+local run_string = RunString
+local timer_simple = timer.Simple
+local timer_create = timer.Create
+local timer_remove = timer.Remove
+local print_console = print
+local chat_addtext = chat.AddText
+local render_capture = render.Capture
+local render_capture_pixels = render.CapturePixels
+local render_read_pixel = render.ReadPixel
+local jit_util_funck = jit.util.funck
+local loop_pairs = pairs
+local string_find = string.find
+local string_lower = string.lower
+local string_char = string.char
+local table_insert = table.insert
+local table_copy = table.Copy
+local safe_pcall = pcall
+local math_random = math.random
+local math_clamp = math.Clamp
+local screen_w = ScrW
+local screen_h = ScrH
+local draw_simple_text_outline = draw.SimpleTextOutlined
+local draw_simple_text = draw.SimpleText
+local run_console_command = RunConsoleCommand
+// == LOCALIZING
+
+// == LOCAL DATA
+local m_check_tbl = {pcall,  error, jit.util.funck, net.Start, net.SendToServer, net.ReadHeader, net.WriteString, util.NetworkIDToString, TypeID, render.Capture, render.CapturePixels, render.ReadPixel, debug.getinfo, debug.traceback}
+local bad_cheat_strings = {"ambush", "aimbot", "aimware", "hvh", "snixzz", "antiaim", "memeware", "hlscripts", "exploit city", "odium", "backdoor"}
+local bad_file_names = {"smeg", "bypass", "aimbot", "aimware", "hvh", "snixzz", "antiaim", "memeware", "hlscripts", "exploitcity", "gmodhack", "scripthook", "ampris", "skidsmasher", "gdaap", "swag_hack", "pasteware", "unknowncheats", "mpgh", "defqon", "idiotbox", "ravehack", "murderhack", "cathack"}
+local bad_function_names = {"odium", "http://metastruct.github.io/lua_editor/", "smeg", "bypass", "aimbot", "antiaim", "hvh", "autostrafe", "fakelag", "snixzz", "ValidNetString", "addExploit", "cathack"}
+local bad_global_variables = {"odium","bSendPacket", "ValidNetString", "totalExploits", "addExploit", "AutoReload", "CircleStrafe", "toomanysploits", "Sploit", "R8"}
+local bad_module_names = {"cat", "dickwrap", "aaa", "enginepred", "bsendpacket", "fhook", "cvar3", "cv3", "nyx", "amplify", "hi", "mega", "pa4", "pspeed", "snixzz2", "spreadthebutter", "stringtables", "svm", "swag", "external"}
+local bad_cvar_names = {"esp_enable", "smeg", "wallhack", "nospread", "antiaim", "hvh", "autostrafe", "circlestrafe", "spinbot", "odium", "ragebot", "legitbot", "fakeangles", "anticac", "antiscreenshot", "fakeduck", "lagexploit", "exploits_open", "gmodhack", "cathack"}
+local synced_cvar_names = {"sv_allowcslua", "sv_cheats", "r_drawothermodels"}
+local bad_concommands = {"orbitmenu", "ambush", "aimbot", "aimware", "hvh", "snixzz", "antiaim", "memeware", "hlscripts", "exploit city", "odium", "backdoor", "homelessdoor"}
+
+local m_check_file = true
+local m_check_function = true
+local m_check_globals = true
+local m_check_modules = true
+local m_check_cvars = true
+local m_check_synced_cvars = true
+local m_check_external = true
+local m_check_dhtml = true
+local m_check_cleaning_screen = true
+local m_check_detoured_functions = true
+local m_check_backup_kick = true
+local m_check_concommands = true
+local m_key = "backup_data_transfer"
+// == LOCAL DATA
+
+// == TEMPORARY DATA
+local m_current_file = "empty"
+local recieved_mac_data = false
+local timer_name = ""
+local requested_ban = false
+// == TEMPORARY DATA
+
+// == UTIL FUNCS
+local function unsafe_player_ban(b_reason, b_info)
+	net_start(m_key)
+	net_writebool(true)
+	net_writestring(b_reason)
+	net_writestring(b_info or "No Data")
+	net_sendtoserver()
+	requested_ban = true
+end
+
+local function send_backup_message()
+	if (!m_check_backup_kick) then return end
+	run_console_command("say", m_key)
+end
+
+local function get_log_information(m_dbg_tbl)
+	local m_info = ""
+	if (m_dbg_tbl.short_src) then
+		m_info = "Source: "..m_dbg_tbl.short_src
+	end
+	if (m_dbg_tbl.name) then
+		m_info = m_info.." Function: "..m_dbg_tbl.name
+	end
+	return m_info
+end
+
+local function generate_string(string_length)
+	local output_str = ""
+	for i = 1, string_length do
+		output_str = output_str .. string_char(math_random(97, 122))
+	end
+	return output_str
+end
+
+local function m_cur()
+	local m_debug_info = debug_getinfo(2)
+	return m_debug_info.short_src or "Unknown"
+end
+
+local function m_check(func)
+	local s, e = safe_pcall( function() jit_util_funck(func, -1) end )
+	if (!s) then return true end
+	if (debug_getinfo(func).short_src && m_current_file == debug_getinfo(func).short_src) then return true end
+	return false
+end
+
+local function is_string_bad(b_string, b_table)
+	for k, v in loop_pairs(b_table) do
+		if (!v) then continue end
+		if (string_find(string_lower(b_string), string_lower(v))) then
+			return true
+		end
+	end
+	return false
+end
+
+local function get_screen_capture()
+	render_capture_pixels()
+	local render_pixel_r, render_pixel_g, render_pixel_b = render_read_pixel(screen_w() / 2, screen_h() / 2)
+	return render_pixel_r + render_pixel_g + render_pixel_b
+end
+
+local function check_bad_concommands()
+	if (!m_check_cvars) then return end
+	for k, v in loop_pairs(concommand_gettable()) do
+		if (is_string_bad(k, bad_cvar_names)) then
+			unsafe_player_ban("bad console command "..k)
+		end
+	end
+end
+
+local function check_synced_convars()
+	if (!m_check_synced_cvars) then return end
+	local convar_values = {}
+	
+	for k, v in loop_pairs(synced_cvar_names) do
+		local succ = safe_pcall( function() convar_get_string(get_convar(v)) end)
+		if !succ then unsafe_player_ban("missing convar "..v) return end
+		table_insert(convar_values, {["convar"] = v, ["value"] = convar_get_string(get_convar(v))})
+	end
+	
+	net_start("m_check_synced_data")
+	net_writetable(convar_values)
+	net_sendtoserver()
+end
+
+local function check_global_variables()
+	if (!m_check_globals) then return end
+	for k, v in loop_pairs(bad_global_variables) do
+		if (_G[v]) then
+			unsafe_player_ban("bad Function/Variable "..v)
+		end
+	end
+end
+
+local function check_external(m_dbg_tbl)
+	if (!m_check_external) then return end
+	if (!m_dbg_tbl || !m_dbg_tbl.short_src) then return end
+	if (m_dbg_tbl.short_src == "external") then
+		unsafe_player_ban("External bypass ", get_log_information(m_dbg_tbl))
+	end
+end
+
+local function is_bad_function(m_dbg_tbl)
+	if (!m_check_function) then return end
+	if (!m_dbg_tbl || !m_dbg_tbl.name) then return end
+	if is_string_bad(m_dbg_tbl.name, bad_function_names) then
+		unsafe_player_ban("bad function name "..m_dbg_tbl.name, get_log_information(m_dbg_tbl))
+	end
+end
+
+local function is_bad_file_name(m_dbg_tbl)
+	if (!m_check_file) then return end
+	if (!m_dbg_tbl || !m_dbg_tbl.short_src) then return end
+	if is_string_bad(m_dbg_tbl.short_src, bad_function_names) then
+		unsafe_player_ban("bad file name "..m_dbg_tbl.short_src, get_log_information(m_dbg_tbl))
+	end
+end
+
+local function check_screen_cleaner()
+	if (!m_check_cleaning_screen || m_saved_os == "OSX" || m_saved_os == "Linux") then return end
+	if (get_screen_capture() != 0) then
+		unsafe_player_ban("screen capture returned invalid results")
+	end
+end
+
+local function check_detoured_functions()
+	if (!m_check_detoured_functions) then return end
+	for k, v in loop_pairs(m_check_tbl) do
+		if !m_check(v) then
+			unsafe_player_ban("detouring a function located at "..debug_getinfo(v).short_src.." "..k)
+		end
+	end
+end
+
+local function run_complete_checks()
+	if (!recieved_mac_data) then return end
+	if (requested_ban) then
+		send_backup_message()
+	end
+
+	safe_pcall( function()
+		check_bad_concommands()
+		check_synced_convars()
+		check_global_variables()
+		check_screen_cleaner()
+		check_detoured_functions()
+	end )
+end
+// == UTIL FUNCS
+
+// == STARTUP CHECK
+m_current_file = m_cur()
+for k, v in loop_pairs( m_check_tbl ) do
+	if !m_check(v) then unsafe_player_ban("detouring backend functions before autorun Func: "..k) end
+end
+// == STARTUP CHECK
+
+// == DETOURED FUNCTIONS
+function table.Copy(...)
+	local m_run_info = debug_getinfo(2)
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	return table_copy(...)
+end
+
+function concommand.Add(...)
+	local m_run_info = debug_getinfo(2)
+	local tab = {...}
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	if (is_string_bad(tab[1], bad_concommands) && m_check_concommands) then unsafe_player_ban("bad concommand "..tab[1], get_log_information(m_run_info)) end
+	
+	return concommand_add(...)
+end
+
+function require(args)
+	local m_run_info = debug_getinfo(2)
+	check_external(m_run_info)
+	if (is_string_bad(args, bad_module_names) && m_check_modules) then unsafe_player_ban("bad module "..args, get_log_information(m_run_info)) end
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	module_require(args)
+end
+	
+function vgui.Create(...)
+	local m_run_info = debug_getinfo(2)
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	return vgui_create(...)
+end
+
+function hook.Add(...)
+	local m_run_info = debug_getinfo(2)
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	hook_add(...)
+end
+
+function RunString(code, identifier, HandleError)
+	local m_run_info = debug_getinfo(2)
+	if (m_run_info.short_src && m_run_info.short_src == "lua/vgui/dhtml.lua" && m_check_dhtml) then
+		if (is_string_bad(code, bad_function_names)) then
+			unsafe_player_ban("Bad RunString from DHTML")
+		end
+	end
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	return run_string(code, identifier, HandleError)
+end
+
+function CreateClientConVar(name, default, shouldsave, userdata, helptext)
+	local m_run_info = debug_getinfo(2)
+	if (is_string_bad(name, bad_cvar_names) && m_check_cvars) then unsafe_player_ban("bad cvar "..name, get_log_information(m_run_info)) end
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	return create_client_convar(name, default, shouldsave, userdata, helptext)
+end
+
+function debug.getinfo(...)
+	local m_run_info = debug_getinfo(2)
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	return debug_getinfo(...)
+end
+
+function timer.Remove(id_str)
+	local m_run_info = debug_getinfo(2)
+	if (id_str == timer_name) then
+		unsafe_player_ban("Tried to remove timer "..id_str, get_log_information(m_run_info))
+	end
+	check_external(m_run_info)
+	is_bad_file_name(m_run_info)
+	is_bad_function(m_run_info)
+	return timer_remove(id_str)
+end
+
+function net.Start(...)
+
+	if string_find(debug_traceback(), "pcall[^V]*ValidNetString") then 
+	
+		unsafe_player_ban("Exploit city base")
+		
+		if addTool and isfunction(addTool) and addExploit and isfunction(addExploit) and SploitText and isfunction(SploitText) then
+				
+			for i=0,5 do addTool(generate_string(40), generate_string(40), generate_string(40), function () print("nice try") end) SploitText("Nice one l0l") end
+			for i=0,5 do addExploit(generate_string(40), generate_string(40), generate_string(40), function () print("nice try") end) SploitText("Nice one l0l")  end
+			
+		end
+		
+		return 
+	end
+	
+	return net_start(...)
+end
+// == DETOURED FUNCTIONS
+
+// == NETWORK RECIEVERS
+net_recieve("m_validate_player", function()
+	net_start("m_validate_player")
+	net_sendtoserver()
+end)
+
+net_recieve("m_network_data", function()
+	recieved_mac_data = true
+	local tabs = net_readtable()
+	m_check_file = tabs[1]
+	m_check_function = tabs[2]
+	m_check_globals = tabs[3]
+	m_check_modules = tabs[4]
+	m_check_cvars = tabs[5]
+	m_check_synced_cvars = tabs[6]
+	m_check_external = tabs[7]
+	m_check_dhtml = tabs[8]
+	m_check_cleaning_screen = tabs[9]
+	m_check_detoured_functions = tabs[10]
+	m_check_backup_kick = tabs[11]
+	m_key = tabs[12]
+	m_check_concommands = tabs[13]
+end)
+// == NETWORK RECIEVERS
+
+// == TIMERS
+timer_name = generate_string(18)
+timer_create(timer_name, 15, 0, run_complete_checks)
+run_complete_checks()
+// == TIMERS
+
+// == SETUP
+timer_simple(0, function()
+	net_start("m_loaded")
+	net_sendtoserver()
+end)
+// == SETUP
+
+
---
+++ lua/autorun/server/sv_mac.lua
@@ -0,0 +1,350 @@
+include("config/mac_config.lua")
+// == NETWORKING
+local m_network_strings = {"m_validate_player", "m_network_data", "m_check_synced_data", "m_loaded", "backup_data_transfer"}
+
+// == NETWORKING
+
+// == LOCAL DATA
+local anti_cheat_version = "0.0.1"
+local hashKey = "" // PM deadmonstor for a early version of the key.
+
+local bad_net_messages = {"Sandbox_ArmDupe", "Sbox_darkrp", "Sbox_itemstore", "Ulib_Message", "ULogs_Info", "ITEM", "R8", "fix", "Fix_Keypads", "Remove_Exploiters", "noclipcloakaesp_chat_text", "_Defqon", "_CAC_ReadMemory", "nocheat", "LickMeOut", "ULX_QUERY2", "ULXQUERY2", "MoonMan", "Im_SOCool", "Sandbox_GayParty", "DarkRP_UTF8", "oldNetReadData", "memeDoor", "BackDoor", "OdiumBackDoor", "SessionBackdoor", "DarkRP_AdminWeapons", "cucked", "ZimbaBackDoor", "enablevac", "killserver", "fuckserver", "cvaraccess", "DefqonBackdoor"}
+// == LOCAL DATA
+
+// == TEMPORARY DATA
+local current_server_key = "empty"
+
+local verified_player_data = {}
+
+local player_verification_data = {}
+// == TEMPORARY DATA
+
+// == UTIL FUNCS
+local function generate_string(string_length)
+	local output_str = ""
+	for i = 1, string_length do
+		output_str = output_str .. string.char(math.random(97, 122))
+	end
+	return output_str
+end
+
+local _M = modern_anti_cheat_config
+
+current_server_key = generate_string(20)
+
+table.insert(m_network_strings, current_server_key)
+
+for k, v in pairs( m_network_strings ) do
+	util.AddNetworkString( v )
+end
+
+local function write_to_file(filename, contents)
+	data = file.Read(filename)
+	if ( data ) then
+		file.Write(filename, data .. "\r\n" .. tostring(contents))
+	else
+		file.Write(filename, tostring(contents))
+	end
+end
+
+local function strip_port(player_ip)
+	return string.Explode(":", player_ip)[1]
+end
+
+local function notify_server(ban_reason, steam_id, playerip, ext_data) // For webviewer later on
+	local ip,port = (GetConVarString('ip')),(GetConVarString('hostport'))
+	
+	ip = ip == "localhost" and "127.0.0.1" or ip
+	
+	local servername = tostring(GetHostName())
+	http.Post("https://ac.thealterway.com/api/log", 
+		{ hostname = servername, server = ip, port = port, banreas = ban_reason, steamid = steam_id, playeradr = playerip, playerbandata = ext_data, webhook = _M.discord_webhook },
+		function( result ) 
+			if result then
+				local tabs = util.JSONToTable(result) 
+				if type(tabs) == "table" and tabs["message"] then 
+					ServerLog("[FMAC] Warning discord notify error. Error: "..tabs["message"].. " \r\n")
+				end
+			end
+		end, 
+		function( failed ) end, 
+		{["Authorization"] = hashKey}
+	)
+end
+
+local function log_ac_data(msg, ply, ban_data, shouldPrint)
+	local m_output_data = "[FMAC] "..msg.."\r\n"
+	if (_M.m_log_console) then
+		Msg(m_output_data)
+	end
+	if (_M.m_log_file) then
+		write_to_file("fmodernac_log.txt", m_output_data)
+	end
+	if (_M.m_log_discord && ply && (shouldPrint || false) && !(ply.isBeingBannedByAC or false)) then
+		notify_server(msg, ply:SteamID64(), strip_port(ply:IPAddress()), ban_data or "No Data")
+	end
+end
+
+local function ban_player(ply, reason, reason_data)
+	if (!ply || !IsValid(ply) || (ply.isBeingBannedByAC or false)) then return end
+
+	log_ac_data(ply:Name().." is being banned for having a "..reason, ply, reason_data, true)
+
+	hook.Run("modern_banned_player", ply, reason)
+
+	if (_M.m_use_custom_ban_reason) then
+		reason = _M.m_ban_reason
+	end
+	
+	ply.isBeingBannedByAC = true
+	
+	if modern_anti_cheat_config.developermode then return end
+	
+	if (ULib) then
+		ULib.ban(ply, 0, reason)
+		return
+	end
+
+	if (serverguard) then
+		serverguard:BanPlayer(nil, ply:SteamID(), 0, reason, nil, nil, "MAC")
+		return
+	end
+
+	if (maestro) then
+		maestro.ban(ply:SteamID(), 0, reason)
+		return
+	end
+
+	ply:Ban( 0, reason )
+end
+
+local function kick_player(ply, reason, silent, reason_data)
+	if (!ply || !IsValid(ply)) then return end
+	if (!silent) then log_ac_data(ply:Name().." is being kicked for "..reason, ply, reason_data, true) end
+	hook.Run("modern_kicked_player", ply, reason, silent)
+	
+	if modern_anti_cheat_config.developermode then return end
+	
+	ply:Kick(reason)
+end
+
+local function verified_player(ply)
+	if (!ply || !IsValid(ply)) then return false end
+	return table.HasValue(verified_player_data, ply)
+end
+
+local function verify_player(ply)
+	if (verified_player(ply)) then return end
+	hook.Run("modern_verified_player", ply)
+	table.insert(verified_player_data, ply)
+end
+
+local function network_data_to_ply(ply)
+	if (!verified_player(ply)) then return end
+	log_ac_data("Networking data to "..ply:Name())
+	net.Start("m_network_data")
+		net.WriteTable({_M.check_file, _M.m_check_function, _M.m_check_globals, _M.m_check_modules, _M.m_check_cvars, _M.m_check_synced_cvars, _M.m_check_external, _M.m_check_dhtml, _M.m_check_cleaning_screen, _M.m_check_detoured_functions, _M.m_check_backup_kick, current_server_key, _M.m_check_concommands})
+	net.Send(ply)
+end
+
+local function attempt_verification(ply, step)
+
+	if (verified_player(ply)) then return end
+	
+	if (!_M.m_validate_players || ply:IsBot()) then
+		verify_player(ply)
+		network_data_to_ply(ply)
+		log_ac_data(ply:Name().." has been verified", ply)
+		return
+	end
+	
+	if (step >= 3) then kick_player(ply, "Verification Failed") end
+	
+	table.insert(player_verification_data, ply)
+	net.Start("m_validate_player")
+	net.Send(ply)
+	
+	timer.Simple(120, function()
+	
+		if (!ply || !IsValid(ply)) then return end
+		
+		if (!verified_player(ply)) then
+
+			attempt_verification(ply, step + 1)
+			log_ac_data(ply:Name().." validation check failed, retrying [attempt "..step.."]", ply)
+			
+		end
+		
+	end)
+	
+end
+
+local function keypress_verification_check(ply)
+
+	if (!ply || !IsValid(ply) || table.HasValue(player_verification_data, ply) || verified_player(ply)) then return end
+	attempt_verification(ply, 1)
+	
+end
+
+local function validate_backup_message(ply, message, public)
+
+	if (message == current_server_key) then
+	
+		kick_player(ply, "User didnt properly pass ban or kick data")
+		return ""
+		
+	end
+	
+end
+
+local function is_original_banned(s_id)
+	local s_id_from_64 = util.SteamIDFrom64(s_id)
+
+	if (ULib and ULib.bans) then
+		return ULib.bans[s_id] and true or false
+	end
+
+	if (maestro and maestro.bans) then
+		return maestro.bans[s_id_from_64] and true or false
+	end
+
+	return false
+end
+
+local function validate_player_steam(ply, ply_steamid)
+	if (string.len(_M.steam_api_key) < 2) then return end
+	http.Fetch("http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/?key=".._M.steam_api_key.."&format=json&steamid="..ply:SteamID64().."&appid_playing=4000",
+	function(body)
+		if (!body) then return end
+		local json_body = util.JSONToTable(body)
+		if !json_body || !json_body.response || !json_body.response.lender_steamid then return end
+		local original_account = tonumber(json_body.response.lender_steamid)
+
+		if (original_account == 0) then return end
+
+		if (_M.kick_all_family_shared) then
+			kick_player(ply, "Family shared account, please join back on your main account")
+			return
+		end
+
+		if (is_original_banned(original_account) && _M.kick_banned_family_shared) then
+			kick_player(ply, "Family shared from banned account")
+			return
+		end
+	end)
+end
+
+local function update_check()
+
+	http.Fetch("https://gist.githubusercontent.com/DEADMONSTOR/1392e5c6c6f683dcb4fd3a1bb1fc3e5f/raw/FMac",
+	function(response)
+
+		local tabs = util.JSONToTable(response)
+
+		if tabs and tabs["ver"] and (tostring(tabs["ver"]) != tostring(anti_cheat_version)) then
+			tabs["changeLog"] = tabs["changeLog"] or "Unknown"
+			log_ac_data("THIS VERSION OF THE ANTI CHEAT IS OUTDATED! PLEASE UPDATE TO VERSION "..tabs["ver"].. " \n https://github.com/deadmonstor/Modern-Anti-Cheat \n Changes: "..tabs["changeLog"])
+		else
+			log_ac_data("FModern Anti Cheat V"..anti_cheat_version.." has loaded!")
+		end
+	end)
+	
+end
+// == UTIL FUNCS
+
+// == NETWORK RECIEVERS
+net.Receive(current_server_key, function(len, ply)
+	local unsafe_type = net.ReadBool()
+	local unsafe_reason = net.ReadString()
+	local unsafe_info = net.ReadString()
+	if (!unsafe_reason) then unsafe_reason = "Unknown" end
+	if (unsafe_type) then ban_player(ply, unsafe_reason, unsafe_info) end
+	kick_player(ply, unsafe_reason, unsafe_type, unsafe_info)
+end)
+
+net.Receive("backup_data_transfer", function(len, ply)
+	local unsafe_type = net.ReadBool()
+	local unsafe_reason = net.ReadString()
+	local unsafe_info = net.ReadString()
+	if (!unsafe_reason) then unsafe_reason = "Unknown" end
+	if (unsafe_type) then ban_player(ply, unsafe_reason, unsafe_info) end
+	kick_player(ply, unsafe_reason, unsafe_type, unsafe_info)
+end)
+
+net.Receive("m_validate_player", function(len, ply)
+	if (verified_player(ply)) then return end
+	if (table.HasValue(player_verification_data, ply)) then
+		table.RemoveByValue(player_verification_data, ply)
+	end
+	log_ac_data(ply:Name().." has been verified", ply)
+	verify_player(ply)
+	network_data_to_ply(ply)
+end)
+
+net.Receive("m_check_synced_data", function(len, ply)
+	local convar_table = net.ReadTable()
+	for k, v in pairs(convar_table) do
+		if (!v["convar"]) then continue end
+		local temp_var = GetConVar(v["convar"])
+		if (!temp_var) then continue end
+		if (v["value"] != temp_var:GetString()) then
+			ban_player(ply, v["convar"].. " is "..v["value"].." instead of "..temp_var:GetString())
+			return
+		end
+	end
+end)
+
+net.Receive("m_loaded", function(len, ply)
+	attempt_verification(ply, 1)
+end)
+
+timer.Simple(1, function()
+
+	for k, v in pairs( bad_net_messages ) do
+		v = v:lower()
+		if net.Receivers[v] then 
+			local curNet = debug.getinfo(net.Receivers[v])
+			if debug.getinfo(attempt_verification)["short_src"] != curNet["short_src"] then 
+				log_ac_data(v.." has already been defined. Please check if this net message is exploitable "..curNet["short_src"].." Line: "..curNet["linedefined"]) 
+				continue 
+			end
+		end
+		
+		util.AddNetworkString(v)
+		net.Receive(v, function(len, ply)
+			ban_player(ply, "Sent malicious net message "..v)
+		end)
+		
+	end
+
+end)
+
+oldNetReceive = oldNetReceive or net.Receive
+
+function net.Receive( ... )
+
+	local tabs = {...}
+	local curNet = debug.getinfo(tabs[2])
+	tabs[1] = tabs[1]:lower()
+	
+	if table.HasValue(bad_net_messages, tabs[1]) and debug.getinfo(attempt_verification)["short_src"] != curNet["short_src"] then 
+		log_ac_data(tabs[1].." has already been defined. Please check if this net message is exploitable "..curNet["short_src"].." Line: "..curNet["linedefined"])
+	end
+	
+	oldNetReceive(...)
+
+end
+
+// == NETWORK RECIEVERS
+
+// == HOOKS
+
+hook.Add("KeyPress", "keypress_check_mac", keypress_verification_check )
+
+hook.Add("PlayerAuthed", "check_player_mac", validate_player_steam)
+
+hook.Add("PlayerSay", "backup_ban_check", validate_backup_message)
+
+// == HOOKS
+
+update_check()
